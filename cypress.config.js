98839f1d56353791f9006521d14ab552ac31ba36
Mahendranath K <mahendranathkundla13@gmail.com>
2024-01-25 11:53:56 +0530

    comitt again

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
